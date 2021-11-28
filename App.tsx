39bb80dd36e85ec266ae5e5ab6c6883038317837
Giorgi Pasieshvili <giorgipasieshvili@Giorgis-MacBook-Pro.local>
2021-11-28 17:54:53 +0400

    added reanimated and gesture handler

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        The first app with bare react native project that also runs on android
-        emulator
-      </Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          The first app with bare react native project that also runs on android
+          emulator
+        </Text>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
